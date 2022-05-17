56d3337b0bdea8e6d59fffe71ac93b46acc7003c
JP2511 <joaopedro.de.mendonca@gmail.com>
2022-05-17 23:13:53 +0200

    Correcting a constraint in the the vertex cover solution using triangle.

--- vertex_cover_lp.py
+++ vertex_cover_lp.py
@@ -13,10 +13,8 @@ from apx import DataFile, LinearProgram, np
 
 def file_reader(filename: str) -> list:
     """Reads a given file.
-
     Args:
         filename (str): path and name of the file.
-
     Returns:
         list(list(str)): lines of the file, where each line is separated in 
             sections by the whitespace.
@@ -32,11 +30,9 @@ def file_reader(filename: str) -> list:
 
 def find_degrees(edges: Iterable) -> dict:
     """Finds the degree of the vertices of a graph.
-
     Args:
         edges (Iterable): pairs of vertices of the graph that correspond to an
             edge.
-
     Returns:
         vertices (dict): vertices with associated degree.
     """
@@ -57,11 +53,9 @@ def find_wedges(edges: Iterable, vert_deg: dict) -> dict:
     """For every pair of vertices that correspond to an edge in the graph, 
     associate lowest degree vertice to all the higher degree vertices that 
     have an edge with it.
-
     Args:
         edges (Iterable): pairs of vertices of the edges of the graph.
         vert_deg (dict): vertices with their associated degrees.
-
     Returns:
         wedges (dict): lowest degree vertices associated to all the highest
             degree vertices with which it forms an edge in the graph.
@@ -79,12 +73,10 @@ def find_wedges(edges: Iterable, vert_deg: dict) -> dict:
 
 def find_triangles(edges: Iterable, wedges: dict) -> set:
     """Finds all the triangles in a graph.
-
     Args:
         edges (Iterable): pairs of vertices that form edges in the graph.
         wedges (dict): lowest degree vertices associated to all the highest
             degree vertices with which it forms an edge in the graph.
-
     Returns:
         triangle (set): triples of vertices that form all triangles (all 
             combinations of two of the three vertices correspond to an edge of
@@ -102,10 +94,8 @@ def find_triangles(edges: Iterable, wedges: dict) -> set:
 def min_buckets(edges: Iterable) -> set:
     """Applies the Minimum Buckets Algorithm (trivial algorithm) to find all
     instances of triangles in a graph.
-
     Args:
         edges (Iterable): pairs of vertices that form edges in the graph.
-
     Returns:
         triangles (set): triples of vertices that form all triangles (all 
             combinations of two of the three vertices correspond to an edge of
@@ -153,7 +143,7 @@ def solve_vertex_cover_triangles(filename: str) -> tuple:
         objective[v] = 1.0
 
     for triangle in min_buckets(edges):
-        vertex_cover_lp.add_constraint({edge: -1 for edge in triangle}, -2)
+        vertex_cover_lp.add_constraint({vert: 1 for vert in triangle}, 2)
     
     vertex_cover_lp.set_objective(objective)
     value, solution = vertex_cover_lp.solve()
